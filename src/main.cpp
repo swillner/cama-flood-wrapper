dd959c0677501dec69dc62aa101864f589ba6ced
Sven Willner <sven.willner@gmail.com>
2019-05-17 17:44:27 +0200

    First attempt for restarting

---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+extern "C" {
+void init_inputnam_mod_mp_init_inputnam_();
+void init_map_mod_mp_init_map_();
+void init_topo_mod_mp_init_topo_();
+void init_cond_mod_mp_init_cond_();
+void init_time_mod_mp_init_time_();
+void control_tstp_mod_mp_control_tstp_();
+void additional_mod_mp_cleanup_tstp_();
+void additional_mod_mp_restart_init_();
+extern int mod_input_mp_irestart_;
+}
+
+int main() {
+  init_inputnam_mod_mp_init_inputnam_();
+  mod_input_mp_irestart_ = 2;
+  init_map_mod_mp_init_map_();
+  init_topo_mod_mp_init_topo_();
+  init_cond_mod_mp_init_cond_();
+  init_time_mod_mp_init_time_();
+  control_tstp_mod_mp_control_tstp_();
+  additional_mod_mp_cleanup_tstp_();
+  //additional_mod_mp_restart_init_();
+  mod_input_mp_irestart_ = 1;
+  init_cond_mod_mp_init_cond_();
+  init_time_mod_mp_init_time_();
+  control_tstp_mod_mp_control_tstp_();
+  return 0;
+}
