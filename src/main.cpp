06bc81f6f00b84f07a7082a2ee4d5ce122507656
Sven Willner <sven.willner@gmail.com>
2019-05-20 09:50:43 +0200

    Fix internal restart

--- src/main.cpp
+++ src/main.cpp
@@ -5,13 +5,16 @@ void init_topo_mod_mp_init_topo_();
 void init_cond_mod_mp_init_cond_();
 void init_time_mod_mp_init_time_();
 void control_tstp_mod_mp_control_tstp_();
+void calc_fldstg_mod_mp_calc_fldstg_();
 void additional_mod_mp_cleanup_tstp_();
 void additional_mod_mp_restart_init_();
 extern int mod_input_mp_irestart_;
+extern int additional_mod_mp_ifirstin_;
 }
 
 int main() {
   init_inputnam_mod_mp_init_inputnam_();
+  additional_mod_mp_ifirstin_ = 0;
   mod_input_mp_irestart_ = 2;
   init_map_mod_mp_init_map_();
   init_topo_mod_mp_init_topo_();
@@ -19,7 +22,8 @@ int main() {
   init_time_mod_mp_init_time_();
   control_tstp_mod_mp_control_tstp_();
   additional_mod_mp_cleanup_tstp_();
-  //additional_mod_mp_restart_init_();
+  additional_mod_mp_restart_init_();
+  additional_mod_mp_ifirstin_ = 0;
   mod_input_mp_irestart_ = 1;
   init_cond_mod_mp_init_cond_();
   init_time_mod_mp_init_time_();
